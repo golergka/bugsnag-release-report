725911ec9df83e1b5372433c36911333bbd90b3c
Max Yankov <max.yankov@glassesoff.com>
2016-02-21 14:15:04 +0200

    Release grouping

--- bugsnag-errors.py
+++ bugsnag-errors.py
@@ -9,6 +9,7 @@ def main():
     parser.add_argument('--api-key', type=str, help='API Key for authorization', required=True, dest='api_key')
     parser.add_argument('--project-id', type=str, help='Project Id', required=True, dest='project_id')
     parser.add_argument('--severity', type=str, help='Issue severity filter', default='error')
+    parser.add_argument('--release-regex', type=str, help='Regex to group version strings by', default=ur'[0-9]*\.[0-9]*\.[0-9]*', dest='release_regex')
     args = parser.parse_args()
 
     sys.stdout.write('Getting issues from bugsnag')
@@ -42,8 +43,13 @@ def main():
 
     errors_by_versions = {}
 
+    import re
+
+    release_filter = re.compile(args.release_regex)
+
     for err in error_list:
-	for ver in err['app_versions'].keys():
+	for v in err['app_versions'].keys():
+	    ver = re.match(release_filter, v).group(0)
 	    if ver in errors_by_versions:
 		errors_by_versions[ver] += 1
 	    else:
@@ -51,7 +57,7 @@ def main():
 
     from tabulate import tabulate
 
-    print tabulate(sorted(errors_by_versions.items()), headers=['Version', 'Errors'])
+    print tabulate(sorted(errors_by_versions.items()), headers=['Release', 'Errors'])
 
 if __name__ == "__main__":
     main()
